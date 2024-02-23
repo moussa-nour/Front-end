7f6b83e107f7a0ca166c5925d486062e4d7f4aff
moussa-nour <moussanour09@gmail.com>
2024-02-23 12:36:21 +0100

    adding file

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import { Provider} from 'react-redux';
 import store from './redux/store';
 import axios from 'axios'
-// axios.defaults.baseURL= "https://todo-api-ql7r.onrender.com/api"
-axios.defaults.baseURL= "http://localhost:5000/api"
+axios.defaults.baseURL= "https://backend-api-rfl9.onrender.com/api"
+// axios.defaults.baseURL= "http://localhost:5000/api"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
